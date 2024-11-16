7c9d8157016343876bc6f6f3c83c9e1bbcefd5cc
Dylan <dylan@mnsr.win>
2024-11-16 01:08:59 -0600

    Finished server and fixed bugs after observing a week in deployment.

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -2,20 +2,42 @@ import random
 import os
 import json
 import time
+import datetime
+import requests
 
+from multiprocessing import Process, Value
+from base64 import b64encode
 from dotenv import load_dotenv
 from paho.mqtt import client as mqtt_client
 
 CPU_TEMP_MAX_C: int = 100
-FREEZER_TEMP_MAX_C: int = -18 * 1000
-FREEZER_TEMP_MIN_C: int = -29 * 1000
-FRIDGE_TEMP_MAX_C: int = 4 * 1000
+FREEZER_TEMP_MAX_C: int = -9
+FREEZER_TEMP_MIN_C: int = -29
+FRIDGE_TEMP_MAX_C: int = 4
 FRIDGE_TEMP_MIN_C: int = 0
 
-last_data = None
-device_online = False
+c_username = ""
+c_password = ""
+c_broker = ""
+c_port = 0
+c_topic = ""
+c_wait_minutes = 0
+c_webhook_url = ""
+c_webhook_auth = ""
+
+multiprocess_timer = None
+device_online = Value('b', False)
 
 def get_config():
+    global c_username
+    global c_password
+    global c_broker
+    global c_port
+    global c_topic
+    global c_wait_minutes
+    global c_webhook_url
+    global c_webhook_auth
+
     load_dotenv()
     try:
         configs = [
@@ -24,40 +46,62 @@ def get_config():
             os.getenv('MQTT_BROKER'),
             os.getenv('MQTT_PORT'),
             os.getenv('MQTT_TOPIC'),
-            os.getenv('MQTT_ACCEPTED_WAIT_MINUTES'),
-            os.getenv('MQTT_WEBHOOK_URL'),
+            os.getenv('MQTT_WAIT_MINUTES'),
+            os.getenv('MATRIX_WEBHOOK_URL'),
+            os.getenv('MATRIX_WEBHOOK_AUTH'),
         ]
+
+        if configs is not None:
+            c_username = configs[0]
+            c_password = configs[1]
+            c_broker = configs[2]
+            c_port = int(configs[3])
+            c_topic = configs[4]
+            c_wait_minutes = configs[5]
+            c_webhook_url = configs[6]
+            c_webhook_auth = configs[7]
+        else:
+            raise Exception("Configuration not found")
+
         return configs
     except Exception as err:
         print(f"Something went wrong with your environment. Please make sure you have every environment variable set properly.\n{err}")
         return None
 
-def print_fahrenheit(celsius_x_1000):
-    celsius = celsius_x_1000 / 1000
+def print_fahrenheit(celsius: float):
     fahrenheit = (celsius * 1.8) + 32
     return round(fahrenheit, 2)
 
+def send_message(url, auth_token, message):
+    print(f"Sending Matrix message: {message}")
 
-def handle_message_new():
-    message = """{"device_id": "Garage Pi", "timestamp": 1730581827.1256046, "sensors": {"cpu_temp": 30.0, "fridge_1": -18657, "freezer_1": -23875, "freezer_2": -22874}}"""
+    auth_header = b64encode(auth_token.encode()).decode()
+    headers = {
+        "Authorization": f"Basic {auth_header}",
+        "Content-Type": "application/json"
+    }
 
-    data = json.loads(message)
+    json_message = json.dumps({"data": message})
 
-    for sensor, value in data['sensors'].items():
-        match sensor:
-            case "cpu_temp":
-                if value > CPU_TEMP_MAX_C:
-                    print(f"CPU is too hot({value} C)")
-            case sensor if "fridge" in sensor:
-                if value > FREEZER_TEMP_MAX_C:
-                    print(f"{sensor} is too hot ({print_fahrenheit(value)} F)")
-                if value < FREEZER_TEMP_MIN_C:
-                    print(f"{sensor} is too cold ({print_fahrenheit(value)} F)")
-            case sensor if "freezer" in sensor:
-                if value > FREEZER_TEMP_MAX_C:
-                    print(f"{sensor} is too hot ({print_fahrenheit(value)} F)")
-                if value < FREEZER_TEMP_MIN_C:
-                    print(f"{sensor} is too cold ({print_fahrenheit(value)} F)")
+    response = requests.post(url, headers=headers, data=json_message)
+    return response
+
+def device_alive_timer():
+    global c_wait_minutes
+
+    wait_seconds = (float(c_wait_minutes) * 60) + 10
+
+    try:
+        time.sleep(wait_seconds)
+        print(f"{c_username} is offline!")
+        send_message(c_webhook_url, c_webhook_auth, f"{c_username} is offline!")
+
+        with device_online.get_lock():
+            device_online.value = False
+
+    except KeyboardInterrupt:
+        print("Killing threads")
+        return
 
 def handle_message(message: str):
     """
@@ -71,32 +115,54 @@ def handle_message(message: str):
     }
     """
 
-    global last_data
-    global device_online
+    global multiprocess_timer
 
     data = json.loads(message)
+    matrix_message: str = ""
+    timestamp = datetime.datetime.fromtimestamp(data['timestamp'])
+    human_readable_time = timestamp.strftime('%m/%d/%Y %l:%M:%S %p')
 
-    if last_data is None:
-        last_data = data
-        device_online = True
-        print("First sighting")
-        print("Current Data:",data)
-    else:
-        #time_diff_between_pings = time.time() - last_data['timestamp']
-        #print(f"There was {time_diff_between_pings} seconds between pings.")
-        print("hey I've seen you before")
-        print("Current Data:", data)
-        print("Last Data:   ", last_data)
-        # do some work
-        last_data = data
+    print(f"\nDevice temps at {human_readable_time}")
+    for sensor, value in data['sensors'].items():
+        sensor_l = sensor.lower()
 
-def device_check_online(self, wait_minutes):
-    wait_seconds = wait_minutes * 60
-    time_diff_between_pings = time.time() - self.last_data['timestamp']
+        try:
+            value = int(value) / 1000
+            match sensor:
+                case "cpu_temp":
+                    if value > CPU_TEMP_MAX_C:
+                        matrix_message += f"\nCPU is too hot({value} C)"
+                case sensor_l if "fridge" in sensor:
+                    if value > FREEZER_TEMP_MAX_C:
+                        matrix_message += f"\n{sensor} is too hot ({print_fahrenheit(value)} F)"
+                    if value < FREEZER_TEMP_MIN_C:
+                        matrix_message += f"\n{sensor} is too cold ({print_fahrenheit(value)} F)"
+                case sensor_l if "freezer" in sensor:
+                    if value > FREEZER_TEMP_MAX_C:
+                        matrix_message += f"\n{sensor} is too hot ({print_fahrenheit(value)} F)"
+                    if value < FREEZER_TEMP_MIN_C:
+                        matrix_message += f"\n{sensor} is too cold ({print_fahrenheit(value)} F)"
 
-    if time_diff_between_pings > wait_seconds:
-        print("Device is offline")
+            print(f"{sensor} is {value} C ({print_fahrenheit(value)} F)")
+        except ValueError:
+            matrix_message += f"\nThere is an error with {sensor}"
 
+    if matrix_message:
+        print("Sending message to Matrix")
+        matrix_message = f"Report from {c_username} at {human_readable_time}{matrix_message}"
+        send_message(c_webhook_url, c_webhook_auth, matrix_message)
+
+    if not device_online.value:
+        with device_online.get_lock():
+            device_online.value = True
+        print(f"{c_username} is online!")
+        send_message(c_webhook_url, c_webhook_auth, f"{c_username} is online!")
+
+    if multiprocess_timer:
+        multiprocess_timer.terminate()
+
+    multiprocess_timer = Process(target=device_alive_timer)
+    multiprocess_timer.start()
 
 def connect_mqtt(username, password, broker, port) -> mqtt_client:
     def on_connect(self, client, userdata, flags, rc):
@@ -113,7 +179,6 @@ def connect_mqtt(username, password, broker, port) -> mqtt_client:
     client.connect(broker, port)
     return client
 
-
 def subscribe(client: mqtt_client, topic):
     def on_message(client, userdata, msg):
         handle_message(msg.payload)
@@ -122,21 +187,9 @@ def subscribe(client: mqtt_client, topic):
     client.on_message = on_message
 
 def main():
-    configs = get_config()
-    if configs is not None:
-        username = configs[0]
-        password = configs[1]
-        broker = configs[2]
-        port = int(configs[3])
-        topic = configs[4]
-        wait_minutes = configs[5]
-        webhook_url = configs[6]
-    else:
-        print("No config provided. Exiting.")
-        return
-
-    client = connect_mqtt(username, password, broker, port)
-    subscribe(client, topic)
+    get_config()
+    client = connect_mqtt(c_username, c_password, c_broker, c_port)
+    subscribe(client, c_topic)
     try:
         client.loop_forever()
     except KeyboardInterrupt:
@@ -144,8 +197,13 @@ def main():
         client.disconnect()
         print("Exiting application")
         return
+    except Exception as err:
+        print(f"Something went wrong: {err}")
+        client.disconnect()
+        if multiprocess_timer:
+            multiprocess_timer.terminate()
+        return
 
 
 if __name__ == '__main__':
-    #main()
-    handle_message_new()
+    main()
